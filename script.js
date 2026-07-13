d2b0a11870da84e4f1ea3dd492162151ad3a3626
F. Daneshjoo <f.daneshjoo12@gmail.com>
2026-07-13 23:51:33 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
-console.log(
+const welcome =
+"Welcome, I'm Farbod Daneshjoo";
+
+
+let i = 0;
+
+
+function typeWelcome(){
+
+
+if(i < welcome.length){
+
+
+document.getElementById("welcome").innerHTML += welcome[i];
+
+i++;
+
+
+setTimeout(typeWelcome,70);
+
+
+}
+
+
+}
+
+
+
+typeWelcome();
+
+
+
+
+
+const text =
+"Software Developer | Open Source Contributor";
+
+
+let j = 0;
+
+
+
+function typing(){
+
+
+if(j < text.length){
+
+
+document.getElementById("typing").innerHTML += text[j];
+
+
+j++;
+
+
+setTimeout(typing,60);
+
+
+}
+
+
+}
+
+
+
+typing();console.log(
 "Welcome to Farbod Daneshjoo Portfolio 🚀"
 );
